ee3baa91dc8371737e62879bceface74884d9245
Mišo <miso@fykos.cz>
2015-04-11 17:23:10 +0200

    css

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,7 @@
-$(function(){
-
+jQuery(function () {
+    var $ = jQuery; 
+    $(window).load(function () {
+        $('.form').css({height: $(window).height()-60});
+        $('textarea').css({height: $(window).height()-130});
+    });
 });
